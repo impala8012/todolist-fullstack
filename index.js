905277af1e3cc846fd5dd51a8748d9b658a6d7c0
Dylan Lo <impala8012@gmail.com>
2021-07-17 21:49:38 +0800

    Add swagger feature for the api doc & move the files to routes

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const pool = require("./db");
+const swaggerUI = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
+const todoRouter = require("./routes/todo");
 
 // middleware
 app.use(cors());
@@ -10,69 +12,27 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-/* =======Route======= */
 
-// Create a todo
-app.post("/todos", async (req, res, next) => {
-  try {
-    const { description } = req.body;
-    const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES($1) RETURNING *",
-      [description]
-    );
-    res.status(201).json(newTodo.rows[0]);
-  } catch (err) {
-    console.log(err.message);
-  }
-});
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "todo API",
+      version: "1.0.0",
+      description: "A todo list express API",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
 
-// Get all todos
-app.get("/todos", async (req, res, next) => {
-  try {
-    const allTodos = await pool.query("SELECT * FROM todo");
-    res.status(200).json(allTodos.rows);
-  } catch (err) {
-    console.log(err.message);
-  }
-});
-// Get a todo
-app.get('/todos/:id', async(req, res, next) => {
-    try {  
-        const {id} = req.params
-        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
-        res.status(200).json(todo.rows)
-    } catch (err) {
-        console.log(err.message)
-    }
-})
-
-
-// update a todo
-app.put('/todos/:id', async(req, res, next)=>{
-    try {
-        const {id} = req.params
-        const {description} = req.body
-        console.log("description", description);
-        const updateTodo = await pool.query(
-          "UPDATE todo SET description = $1 WHERE todo_id = $2",
-          [description, id]
-        );
-        res.status(204).send("Todo was updated")
-    } catch (err) {
-        console.log(err.message)
-    }
-})
-// delete a todo
-app.delete("/todos/:id", async(req, res, next) => {
-    try {
-        const {id} = req.params
-        const deletedTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1",[id])
-        res.status(204).send("Todo was deleted")
-    } catch (err){
-        console.log(err.message)
-    }
-})
-
+const specs = swaggerJsDoc(options)
+app.use("/api-docs", swaggerUI.serve,swaggerUI.setup(specs))
+app.use("/todos", todoRouter);
 
 app.listen(5000, () => {
   console.log("server has started listen on port 5000");
---
+++ routes/todo.js
@@ -0,0 +1,219 @@
+const router = require("express").Router();
+const pool = require("../db");
+
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Todos:
+ *       type: object
+ *       required:
+ *         - id
+ *         - description
+ *       properties:
+ *         id:
+ *           type: number
+ *           description: The auto-generated id of the todo
+ *         description:
+ *           type: string
+ *           description: the todo list
+ *       example:
+ *         id: 2
+ *         description: walk the dogs
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Todos
+ *   description: The todos managing API
+ */
+
+/**
+ * @swagger
+ *  /todos:
+ *      post:
+ *          summary: Create a new todo
+ *          tags: [Todos]
+ *          requestBody: 
+ *              required: true
+ *              content: 
+ *                  application/json: 
+ *                      schema: 
+ *                          $ref: '#/components/schemas/Todos'
+ *          responses:
+ *              201:
+ *                  description: The todo was successfully created
+ *                  content: 
+ *                      application/json: 
+ *                          schema: 
+ *                              $ref: '#/components/schemas/Todos'
+ *              500: 
+ *                  description: Some server error
+ */
+
+// Create a todo
+router.post("/", async (req, res, next) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.status(201).json(newTodo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+/**
+ * @swagger
+ *  /todos:
+ *      get:
+ *          summary: Get all todos
+ *          tags: [Todos]
+ *          responses:
+ *              200:
+ *                  description: The list of the todos
+ *                  content: 
+ *                      application/json: 
+ *                          schema: 
+ *                              type: array
+ *                              items: 
+ *                                  $ref: '#/components/schemas/Todos'
+ *              500: 
+ *                  description: Some server error
+ */
+
+
+// Get all todos
+router.get("/", async (req, res, next) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.status(200).json(allTodos.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+/**
+ * @swagger
+ *  /todos/{id}: 
+ *      get:
+ *          summary: Get todo by id
+ *          tags: [Todos]
+ *          parameters: 
+ *              - in: path
+ *                name: id
+ *                schema: 
+ *                  type: string
+ *                required: true
+ *                description: The todo id
+ *          responses:
+ *              200:
+ *                  description: The todo by id
+ *                  content: 
+ *                      application/json: 
+ *                          schema: 
+ *                              $ref: '#/components/schemas/Todos'
+ *              404: 
+ *                  description: Todo was not found
+ */
+// Get a todo
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.status(200).json(todo.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+/**
+ * @swagger
+ *  /todos/{id}: 
+ *      put:
+ *          summary: Update todo by id
+ *          tags: [Todos]
+ *          parameters: 
+ *              - in: path
+ *                name: id
+ *                schema: 
+ *                  type: string
+ *                required: true
+ *                description: The todo id
+ *          requestBody: 
+ *              required: true
+ *              content: 
+ *                  application/json: 
+ *                      schema: 
+ *                          $ref: '#/components/schemas/Todos'
+ *          responses:
+ *              20$:
+ *                  description: The todo was updated
+ *                  content: 
+ *                      application/json: 
+ *                          schema: 
+ *                              $ref: '#/components/schemas/Todos'
+ *              404: 
+ *                  description: Todo was not found
+ *              500: 
+ *                  description: Some error happened
+ */
+
+// update a todo
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    console.log("description", description);
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.status(204).send("Todo was updated");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+
+/**
+ * @swagger
+ *  /todos/{id}: 
+ *      delete:
+ *          summary: Delete todo by id
+ *          tags: [Todos]
+ *          parameters: 
+ *              - in: path
+ *                name: id
+ *                schema: 
+ *                  type: string
+ *                required: true
+ *                description: The todo id
+ *          responses:
+ *              204:
+ *                  description: The todo was deleted 
+ *              404: 
+ *                  description: Todo was not found
+ */
+// delete a todo
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedTodo = await pool.query(
+      "DELETE FROM todo WHERE todo_id = $1",
+      [id]
+    );
+    res.status(204).send("Todo was deleted");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+module.exports = router;
